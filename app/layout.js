8e2577e25bd08d430e85cfd69018952929dfb7fe
anonysuiit-cloud <anonysuiit@gmail.com>
2026-08-15 19:47:48 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'Hostel Mess Board',
+  description: 'Exchange extra mess meals',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-gray-50 min-h-screen">{children}</body>
+    </html>
+  )
+}
